93ef86edd89331cde3c01742606cb14eb0a2d01d
EPICPLAYER1708 <88785601+EPICPLAYER1708@users.noreply.github.com>
2021-12-27 19:52:48 +0530

    Add files via upload

---
+++ Game/sketch.js
@@ -0,0 +1,44 @@
+var canvas
+var bgImg;
+var bob; // main character
+var dude;  // main boss
+var nix; // semi boss 1
+var buck; // semi boss 2 selected
+var big; // semi boss 3
+var tony; // semi boss 4 selected
+var fine; // semi boss 5
+var amul; // semi boss 6 selected
+var jade; //semi boss 7
+var sky; //semi boss 8
+var bet; //semiboss 9
+var eye; //semiboss 10 selected
+var gems; //semiboss 11
+
+function preload(){ 
+  bgImg.loadImage("Backround.png");  
+  bob.addImage("bob.png");
+  dude.addImage();
+  nix.addImage();
+  buck.addImage();
+  big.addImage();
+  tony.addImage();
+  fine.addImage();
+  amul.addImage();
+  jade.addImage();
+  sky.addImage();
+  bet.addImage();
+  eye.addImage();
+  gems.addImage();
+}
+
+function setup() {
+ canvas = createCanvas(windowWidth, windowHeight);
+ 
+ 
+}
+
+function draw() {
+ 
+  
+
+}
